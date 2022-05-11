642bdd5e18e54b170587b0402f2271db1b8cff7c
amr farrag <eng.ame.farrag@outlook.com>
2022-05-11 08:58:34 +0200

    add replacement

--- src/com/isfp/projectModel.groovy
+++ src/com/isfp/projectModel.groovy
@@ -4,13 +4,23 @@ import groovy.json.JsonSlurper
 class ProjectModel implements Serializable {
     String projectName;
     String buildNode;
-    String buildDirectory;
     String appDirectory;
+    String backupPath;
+    String patchPath;
+    String sourceLocation;
+    String[] blockList;
+    def replacement ;
+
     ProjectModel(String name){
         def jsonSlurper = new JsonSlurper()
-        def config = jsonSlurper.parse(new File('../resources/config.json'))
+        def config = jsonSlurper.parse(new File('../resources/bulid.json'))
+        def project = config[name]
         this.projectName=name
-        this.buildNode=config[name].buildNode
-        this.buildDirectory=config[name].buildDirectory
+        this.buildNode=project.buildNode
+        this.sourceLocation=project.sourceLocation
+        this.replacement=project.replacement
+        this.appDirectory=project.appDirectory
+        this.patchPath=project.patchPath
+        this.patchPath=project.backupPath
     }
  }
