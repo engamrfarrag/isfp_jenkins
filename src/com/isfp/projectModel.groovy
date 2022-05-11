b96486fb0a61a68da5c539e697b6c223add261ad
amr farrag <eng.ame.farrag@outlook.com>
2022-05-11 09:53:11 +0200

    update project model serilzation of dynamic types

--- src/com/isfp/projectModel.groovy
+++ src/com/isfp/projectModel.groovy
@@ -1,7 +1,7 @@
 package com.isfp
-import groovy.json.JsonSlurper
-
 class ProjectModel implements Serializable {
+    ProjectModel(){
+    }
     String projectName;
     String buildNode;
     String appDirectory;
@@ -9,18 +9,5 @@ class ProjectModel implements Serializable {
     String patchPath;
     String sourceLocation;
     String[] blockList;
-    def replacement ;
-
-    ProjectModel(String name){
-        def jsonSlurper = new JsonSlurper()
-        def config = jsonSlurper.parse(new File('../resources/bulid.json'))
-        def project = config[name]
-        this.projectName=name
-        this.buildNode=project.buildNode
-        this.sourceLocation=project.sourceLocation
-        this.replacement=project.replacement
-        this.appDirectory=project.appDirectory
-        this.patchPath=project.patchPath
-        this.patchPath=project.backupPath
-    }
+    //def replacement ;
  }
